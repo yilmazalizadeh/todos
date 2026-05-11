80faf329b584cab28ab7b16d9ae015d81501dd0b
Yilmaz Alizadeh <yilmaz.alizadeh@trustflight.com>
2026-05-11 10:10:21 +0100

    Set up project scaffolding and testing framework, added initial DbContext, In-Memory database, and unit test for CRUD operations.

---
+++ TravelApp.Test/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TravelApp.Test;
+
+public class UnitTest1
+{
+    private TravelDbContext GetDbContext()
+    {
+        var options = new DbContextOptionsBuilder<TravelDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+        return new TravelDbContext(options);
+    }
+
+    [Fact]
+    public async Task CRUD_Operations_Work()
+    {
+        var db = GetDbContext();
+        var todosApi = new TodosApi(db);
+
+        // Create
+        var newTodo = new Todo { Title = "Test Todo", IsComplete = false };
+        var created = await todosApi.CreateTodo(newTodo);
+        Assert.NotEqual(0, created.Id);
+
+        // Read
+        var fetched = await todosApi.GetTodoById(created.Id);
+        Assert.NotNull(fetched);
+        Assert.Equal("Test Todo", fetched.Title);
+
+        // Update
+        fetched.Title = "Updated Title";
+        await todosApi.UpdateTodo(fetched);
+        var updated = await todosApi.GetTodoById(created.Id);
+        Assert.Equal("Updated Title", updated.Title);
+
+        // Delete
+        await todosApi.DeleteTodo(created.Id);
+        var deleted = await todosApi.GetTodoById(created.Id);
+        Assert.Null(deleted);
+    }
+}
---
+++ TravelApp/TravelDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class TravelDbContext : DbContext
+{
+    public TravelDbContext(DbContextOptions<TravelDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Todo> Todos => Set<Todo>();
+}
